1189b01f09f2c2bf6dd1532ce0773eda659233e5
gerti <algertsuta@gmail.com>
2022-09-29 12:14:28 +0200

    vertical centrato

--- src/components/MyPager.js
+++ src/components/MyPager.js
@@ -5,10 +5,10 @@ import PagerView from 'react-native-pager-view';
 export const MyPager = () => {
   return (
     <PagerView style={styles.pagerView}  orientation="vertical" initialPage={0}	>
-      <View key="1" collapsable={false} style={{backgroundColor: 'red'}}>
+      <View key="1" collapsable={false} style={[styles.pg,{backgroundColor: 'red'}]}>
         <Text>First page</Text>
       </View>
-      <View key="2" collapsable={false}>
+      <View key="2" collapsable={false} style={styles.pg}>
         <Text>Second page</Text>
       </View>
     </PagerView>
@@ -19,6 +19,10 @@ const styles = StyleSheet.create({
   pagerView: {
     height: '100%',
     width:'100%',
-    
   },
+  pg:{
+    flex:1,
+    justifyContent:'center',
+    alignItems: 'center'
+  }
 });
